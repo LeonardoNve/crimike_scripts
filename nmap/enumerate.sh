fbbede9df6e89a0c1b3fb3012898552da1920aa8
crimike <cristi@localhost.localdomain>
2014-12-22 17:21:04 +0000

    Added .pl to udp-proto-scanner calling

--- nmap/enumerate.sh
+++ nmap/enumerate.sh
@@ -19,7 +19,7 @@ while read ip; do
     echo "Simple UDP Scan on: $ip"
     sudo nmap -Pn -v -A -sU --top-ports 200 -oA ${ip}_simple_udp $ip > /dev/null
     echo "udp-proto-scanner on: $ip"
-    sudo udp-proto-scanner $ip > ${ip}_proto.udp
+    sudo udp-proto-scanner.pl $ip > ${ip}_proto.udp
 done<$1
 
 echo "Running full TCP scan on list"
--- nmap/hostDiscovery.sh
+++ nmap/hostDiscovery.sh
@@ -5,5 +5,5 @@ sudo nmap -sn -PS80,22,56273 -PA80,22,49163 -PU53,12739 -PP -PM -PR --excludefil
 echo "Running blind host discovery:"
 sudo nmap -Pn --top-ports 500 --excludefile excludedIps $1 > blindtcp.discovery
 echo "Running udp-proto-scanner"
-sudo nmap -sL --excludefile excludedIps $1 | ./getIpsFromFile.sh | xargs | udp-proto-scanner | ./getIpsFromFile.sh | sort | uniq > udp.discovery
+sudo nmap -sL --excludefile excludedIps $1 | ./getIpsFromFile.sh | xargs | udp-proto-scanner.pl | ./getIpsFromFile.sh | sort | uniq > udp.discovery
 mv excludedIps discovered.hosts
